57ea54dd1462c82c849aa71a0d77ae48aa3070c4
suwan <jsuwan961205@gmail.com>
2021-10-25 18:07:50 +0900

    git add train components

---
+++ kubeflow_pipeline/2_train_model/model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+
+def build_mnist_model():
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    return model
---
+++ kubeflow_pipeline/2_train_model/run.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.lib.io import file_io
+import os
+from model import build_mnist_model
+
+BUCKET_DIR = "gs://suwan_bucket"
+PROC_DATASET_DIR = "mnist/proc"
+MODEL_DIR = "mnist/model"
+
+train_x = np.load(
+    file_io.FileIO(os.path.join(BUCKET_DIR, PROC_DATASET_DIR, "train_x.npy"), "rb")
+)
+train_y = np.load(
+    file_io.FileIO(os.path.join(BUCKET_DIR, PROC_DATASET_DIR, "train_y.npy"), "rb")
+)
+test_x = np.load(
+    file_io.FileIO(os.path.join(BUCKET_DIR, PROC_DATASET_DIR, "test_x.npy"), "rb")
+)
+test_y = np.load(
+    file_io.FileIO(os.path.join(BUCKET_DIR, PROC_DATASET_DIR, "test_y.npy"), "rb")
+)
+
+model = build_mnist_model()
+
+model.fit(train_x, train_y, epochs=3)
+
+model.save(os.path.join(BUCKET_DIR, MODEL_DIR))
